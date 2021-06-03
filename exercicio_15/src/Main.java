2939ee1a38a2cc42e8b60c02795e7285c2c4cd9e
Marcio Souza <marciodougas.camargo@gmail.com>
2021-06-02 23:14:27 -0300

    exercicios_while

---
+++ exercicio_15/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int Senha = sc.nextInt();
+		
+		while(Senha != 2002) {
+			System.out.println("Senha inválida");
+			Senha = sc.nextInt();
+		}
+			System.out.println("Acesso Permitido");
+					
+		sc.close();
+		
+	}
+
+}
